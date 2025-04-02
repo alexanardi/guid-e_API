19dd7b00bad6eab904868189c850587d3681f40e
Alex Castro Torres <acastro@keeptolearn.local>
2025-04-01 21:48:23 -0300

    Agrega middleware CORS para permitir frontend local http y https

--- app/main.py
+++ app/main.py
@@ -9,7 +9,10 @@ app = FastAPI()
 # 🔒 Middleware CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # solo frontend local
+    allow_origins=[
+        "http://localhost:3000",
+        "https://localhost:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
